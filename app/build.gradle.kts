b8ae05c72fd85672c5e8670a28a701f43950e313
wingio <wingio@users.noreply.github.com>
2023-05-14 19:51:27 -0400

    Chore: Bump agp and app version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "dev.beefers.vendetta.manager"
         minSdk = 28
         targetSdk = 33
-        versionCode = 1080
-        versionName = "1.0.8"
+        versionCode = 1081
+        versionName = "1.0.81"
 
         buildConfigField("String", "GIT_BRANCH", "\"${getCurrentBranch()}\"")
         buildConfigField("String", "GIT_COMMIT", "\"${getLatestCommit()}\"")
