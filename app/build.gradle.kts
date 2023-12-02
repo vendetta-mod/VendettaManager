d6929c18b910c4d01a432dc3e8324b5f184574d2
Wing <44992537+wingio@users.noreply.github.com>
2023-12-02 17:05:05 -0500

    Bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "dev.beefers.vendetta.manager"
         minSdk = 28
         targetSdk = 34
-        versionCode = 1097
-        versionName = "1.0.97"
+        versionCode = 1098
+        versionName = "1.0.98"
 
         buildConfigField("String", "GIT_BRANCH", "\"${getCurrentBranch()}\"")
         buildConfigField("String", "GIT_COMMIT", "\"${getLatestCommit()}\"")
